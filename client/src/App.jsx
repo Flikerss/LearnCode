d62b9a6a638ac74a8e95e88a668a2f3e93a7b814
amoui <artimediw37916002@gmail.com>
2025-10-21 12:54:23 +1000

    signin signup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import Navbar from "./components/Navbar";
+import SignIn from "./pages/SignIn/SignIn";
+import SignUp from "./pages/SingUp/SignUp";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/login" element={<SignIn />} />
+        <Route path="/register" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   );
